cf1ed8a44661ba64519af3dea1acc81168c7c146
Ryzycki,Samuel <samuel.ryzycki@vanguard.co.uk>
2020-08-18 19:26:02 +0100

    Chapter 2 - Auto-transform Payloads to DTO instances

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -17,8 +17,8 @@ export class CoffeesService {
     return this.coffees;
   }
 
-  findOne(id: string) {
-    const coffee = this.coffees.find(item => item.id === +id);
+  findOne(id: number) {
+    const coffee = this.coffees.find(item => item.id === id);
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
     }
@@ -31,15 +31,15 @@ export class CoffeesService {
     return createCoffeeDto;
   }
 
-  update(id: string, updateCoffeeDto: any) {
+  update(id: number, updateCoffeeDto: any) {
     const existingCoffee = this.findOne(id);
     if (existingCoffee) {
       // update the existing entity
     }
   }
 
-  remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
+  remove(id: number) {
+    const coffeeIndex = this.coffees.findIndex(item => item.id === id);
     if (coffeeIndex >= 0) {
       this.coffees.splice(coffeeIndex, 1);
     }
